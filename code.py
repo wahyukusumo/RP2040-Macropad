7709dac5319e38511ae55e2a4d81a2ca602cc1e0
Wahyu Kusumo <dev@wahyukusumo.com>
2025-04-01 19:02:59 +0700

    Move code about buttons to different function since it will be rewrite later

--- code.py
+++ code.py
@@ -28,52 +28,85 @@ mouse = Mouse(usb_hid.devices)
 cc = ConsumerControl(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
-# list of pins to use (skipping GP15 on Pico because it's funky)
-pins = [
-    board.GP6,
-    board.GP7,
-    board.GP8,
-    board.GP9,
-    board.GP10,
-    board.GP11,
-    board.GP12,
-    board.GP13,
-    board.GP14,
-    board.GP15,
-    board.GP17,
-    board.GP18,
-]
 
-total_pins = len(pins)
+# This code is for buttons, currently moved to this function since it will be rewrite later.
+def button_unused():
+    # list of pins to use (skipping GP15 on Pico because it's funky)
+    pins = [
+        board.GP6,
+        board.GP7,
+        board.GP8,
+        board.GP9,
+        board.GP10,
+        board.GP11,
+        board.GP12,
+        board.GP13,
+        board.GP14,
+        board.GP15,
+        board.GP17,
+        board.GP18,
+    ]
 
-MEDIA = 1  # this can be for volume, media player, brightness etc.
-KEY = 2
-STRING = 3
-NEW_LINE = "NEW_LINE"
+    total_pins = len(pins)
 
-keymap = {
-    (0): (KEY, [Keycode.TWO]),
-    (1): (KEY, [Keycode.FIVE]),
-    (2): (KEY, [Keycode.C]),
-    (3): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.ONE]),
-    (4): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.TWO]),
-    (5): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.THREE]),
-    (6): (KEY, [Keycode.M]),
-    (7): (KEY, [Keycode.F]),
-    (8): (KEY, [Keycode.I]),
-    (9): (KEY, [Keycode.SPACE]),
-    (10): (KEY, [Keycode.LEFT_CONTROL, Keycode.Z]),
-    (11): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.Z]),
-}
+    MEDIA = 1  # this can be for volume, media player, brightness etc.
+    KEY = 2
+    STRING = 3
+    NEW_LINE = "NEW_LINE"
 
-switches = [0] * total_pins
+    keymap = {
+        (0): (KEY, [Keycode.TWO]),
+        (1): (KEY, [Keycode.FIVE]),
+        (2): (KEY, [Keycode.C]),
+        (3): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.ONE]),
+        (4): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.TWO]),
+        (5): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.THREE]),
+        (6): (KEY, [Keycode.M]),
+        (7): (KEY, [Keycode.F]),
+        (8): (KEY, [Keycode.I]),
+        (9): (KEY, [Keycode.SPACE]),
+        (10): (KEY, [Keycode.LEFT_CONTROL, Keycode.Z]),
+        (11): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.Z]),
+    }
 
-for i in range(total_pins):
-    switches[i] = DigitalInOut(pins[i])
-    switches[i].direction = Direction.INPUT
-    switches[i].pull = Pull.UP
+    switches = [0] * total_pins
 
-switch_state = [0] * total_pins
+    for i in range(total_pins):
+        switches[i] = DigitalInOut(pins[i])
+        switches[i].direction = Direction.INPUT
+        switches[i].pull = Pull.UP
+
+    switch_state = [0] * total_pins
+
+    while True:
+        # Handle the 12 button switches
+        for button in range(total_pins):
+            if switch_state[button] == 0:
+                if not switches[button].value:  # Button pressed
+                    try:
+                        if keymap[button][0] == KEY:
+                            kbd.press(*keymap[button][1])
+                        elif keymap[button][0] == STRING:
+                            for letter in keymap[button][1][0]:
+                                layout.write(letter)
+                            if keymap[button][1][1] == NEW_LINE:
+                                kbd.press(*[Keycode.RETURN])
+                                kbd.release(*[Keycode.RETURN])
+                        else:
+                            cc.send(keymap[button][1][0])
+                    except ValueError:  # deals with six-key limit
+                        pass
+                    switch_state[button] = 1
+
+            if switch_state[button] == 1:
+                if switches[button].value:  # Button released
+                    try:
+                        if keymap[button][0] == KEY:
+                            kbd.release(*keymap[button][1])
+                    except ValueError:
+                        pass
+                    switch_state[button] = 0
+
 
 encoder_1 = rotaryio.IncrementalEncoder(board.GP0, board.GP1)
 encoder_2 = rotaryio.IncrementalEncoder(board.GP2, board.GP3)
@@ -84,33 +117,6 @@ last_position_2 = encoder_2.position
 last_position_3 = encoder_3.position
 
 while True:
-    # Handle the 12 button switches
-    for button in range(total_pins):
-        if switch_state[button] == 0:
-            if not switches[button].value:  # Button pressed
-                try:
-                    if keymap[button][0] == KEY:
-                        kbd.press(*keymap[button][1])
-                    elif keymap[button][0] == STRING:
-                        for letter in keymap[button][1][0]:
-                            layout.write(letter)
-                        if keymap[button][1][1] == NEW_LINE:
-                            kbd.press(*[Keycode.RETURN])
-                            kbd.release(*[Keycode.RETURN])
-                    else:
-                        cc.send(keymap[button][1][0])
-                except ValueError:  # deals with six-key limit
-                    pass
-                switch_state[button] = 1
-
-        if switch_state[button] == 1:
-            if switches[button].value:  # Button released
-                try:
-                    if keymap[button][0] == KEY:
-                        kbd.release(*keymap[button][1])
-                except ValueError:
-                    pass
-                switch_state[button] = 0
 
     # Handle Rotary Encoder 1 (track rotation and send arrow keys)
     last_position_1 = handle_encoder(encoder_1, last_position_1, "Encoder 1")
