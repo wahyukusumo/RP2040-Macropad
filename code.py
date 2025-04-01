7b491f589400d1539dc7c9acaf0d9132b5500f5b
Wahyu Kusumo <dev@wahyukusumo.com>
2025-04-01 21:14:48 +0700

    Create Button class and extend it to RotaryEncoder class

--- code.py
+++ code.py
@@ -10,6 +10,21 @@ from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
+kbd = Keyboard(usb_hid.devices)
+mouse = Mouse(usb_hid.devices)
+cc = ConsumerControl(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
+
+MEDIA = 1  # this can be for volume, media player, brightness etc.
+KEY = 2  # Keyboard press
+STRING = 3  # Text string
+WRAPPED = 4
+NEW_LINE = "NEW_LINE"
+
+
+def handle_input_action():
+    pass
+
 
 def handle_encoder(encoder, last_position, name):
     current_position = encoder.position
@@ -23,109 +38,21 @@ def handle_encoder(encoder, last_position, name):
     return current_position
 
 
-kbd = Keyboard(usb_hid.devices)
-mouse = Mouse(usb_hid.devices)
-cc = ConsumerControl(usb_hid.devices)
-layout = KeyboardLayoutUS(kbd)
-
-MEDIA = 1  # this can be for volume, media player, brightness etc.
-KEY = 2  # Keyboard press
-STRING = 3  # Text string
-WRAPPED = 4
-NEW_LINE = "NEW_LINE"
-
-
-# This code is for buttons, currently moved to this function since it will be rewrite later.
-def button_unused():
-    # list of pins to use (skipping GP15 on Pico because it's funky)
-    pins = [
-        board.GP6,
-        board.GP7,
-        board.GP8,
-        board.GP9,
-        board.GP10,
-        board.GP11,
-        board.GP12,
-        board.GP13,
-        board.GP14,
-        board.GP15,
-        board.GP17,
-        board.GP18,
-    ]
-
-    total_pins = len(pins)
-
-    keymap = {
-        (0): (KEY, [Keycode.TWO]),
-        (1): (KEY, [Keycode.FIVE]),
-        (2): (KEY, [Keycode.C]),
-        (3): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.ONE]),
-        (4): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.TWO]),
-        (5): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.THREE]),
-        (6): (KEY, [Keycode.M]),
-        (7): (KEY, [Keycode.F]),
-        (8): (KEY, [Keycode.I]),
-        (9): (KEY, [Keycode.SPACE]),
-        (10): (KEY, [Keycode.LEFT_CONTROL, Keycode.Z]),
-        (11): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.Z]),
-    }
-
-    switches = [0] * total_pins
-
-    for i in range(total_pins):
-        switches[i] = DigitalInOut(pins[i])
-        switches[i].direction = Direction.INPUT
-        switches[i].pull = Pull.UP
-
-    switch_state = [0] * total_pins
-
-    while True:
-        # Handle the 12 button switches
-        for button in range(total_pins):
-            if switch_state[button] == 0:
-                if not switches[button].value:  # Button pressed
-                    try:
-                        if keymap[button][0] == KEY:
-                            kbd.press(*keymap[button][1])
-                        elif keymap[button][0] == STRING:
-                            for letter in keymap[button][1][0]:
-                                layout.write(letter)
-                            if keymap[button][1][1] == NEW_LINE:
-                                kbd.press(*[Keycode.RETURN])
-                                kbd.release(*[Keycode.RETURN])
-                        else:
-                            cc.send(keymap[button][1][0])
-                    except ValueError:  # deals with six-key limit
-                        pass
-                    switch_state[button] = 1
-
-            if switch_state[button] == 1:
-                if switches[button].value:  # Button released
-                    try:
-                        if keymap[button][0] == KEY:
-                            kbd.release(*keymap[button][1])
-                    except ValueError:
-                        pass
-                    switch_state[button] = 0
-
-
-class RotaryEncoder:
-    def __init__(self, name: str, pin_a, pin_b, pin_button):
-        self.name = name
-        self.encoder = rotaryio.IncrementalEncoder(pin_a, pin_b)
-        self.last_position = self.encoder.position
-        self.encoder_button = self.init_button(pin_button)
+class Button:
+    def __init__(self, pin_button: board.Pin):
+        # def __init__(self, pin_button: board.Pin):
+        self.button = self.init_button(pin_button)
         self.button_state = 0
 
-    def init_button(self, pin_button):
+    def init_button(self, pin_button: board.Pin):
         button = DigitalInOut(pin_button)
         button.direction = Direction.INPUT
         button.pull = Pull.UP  # Pull-up resistor enabled
         return button
 
-    def handle_button(self, keymap):
+    def handle_button(self, keymap: tuple):
         if self.button_state == 0:
-            if not self.encoder_button.value:
+            if not self.button.value:
                 try:
                     if keymap[0] == KEY:
                         kbd.press(*keymap[1])
@@ -142,7 +69,7 @@ class RotaryEncoder:
                 self.button_state = 1
 
         if self.button_state == 1:
-            if self.encoder_button.value:
+            if self.button.value:
                 try:
                     if keymap[0] == KEY:
                         kbd.release(*keymap[1])
@@ -150,38 +77,56 @@ class RotaryEncoder:
                     pass
                 self.button_state = 0
 
+
+class RotaryEncoder(Button):
+    def __init__(
+        self, name: str, pin_a: board.Pin, pin_b: board.Pin, pin_button: board.Pin
+    ):
+        super().__init__(pin_button)
+        self.name = name
+        self.encoder = rotaryio.IncrementalEncoder(pin_a, pin_b)
+        self.last_position = self.encoder.position
+
     def handle_encoder(self):
         current_position = self.encoder.position
         if current_position != self.last_position:
             if current_position > self.last_position:
                 print(f"{self.name}: Clockwise")
-                print(current_position)
                 mouse.move(wheel=1)
             else:
                 print(f"{self.name}: Counterclockwise")
-                print(current_position)
                 mouse.move(wheel=-1)
             self.last_position = current_position
 
 
-encoder = RotaryEncoder("Encoder 1", board.GP0, board.GP1, board.GP5)
+def main():
+    # Initialize your encoders
+    # encoder_1 = rotaryio.IncrementalEncoder(board.GP0, board.GP1)
+    encoder_2 = rotaryio.IncrementalEncoder(board.GP3, board.GP4)
+    encoder_3 = rotaryio.IncrementalEncoder(board.GP12, board.GP13)
+    encoder_4 = rotaryio.IncrementalEncoder(board.GP16, board.GP17)
 
-# encoder_1 = rotaryio.IncrementalEncoder(board.GP0, board.GP1)
-encoder_2 = rotaryio.IncrementalEncoder(board.GP2, board.GP3)
-encoder_3 = rotaryio.IncrementalEncoder(board.GP4, board.GP5)
+    # Track positions separately
+    # last_position_1 = encoder_1.position
+    last_position_2 = encoder_2.position
+    last_position_3 = encoder_3.position
+    last_position_4 = encoder_4.position
 
-# last_position_1 = encoder_1.position
-last_position_2 = encoder_2.position
-last_position_3 = encoder_3.position
+    encoder = RotaryEncoder("Encoder 1", board.GP0, board.GP1, board.GP5)
+    print(type(encoder.button))
 
-while True:
-    encoder.handle_encoder()
-    encoder.handle_button((KEY, [Keycode.LEFT_CONTROL, Keycode.Z]))
+    while True:
+        # Call handle_encoder() for each encoder, one after the other
+        # last_position_1 = handle_encoder(encoder_1, last_position_1, "Encoder 1")
+        encoder.handle_encoder()
+        encoder.handle_button((KEY, [Keycode.C]))
+        last_position_2 = handle_encoder(encoder_2, last_position_2, "Encoder 2")
+        last_position_3 = handle_encoder(encoder_3, last_position_3, "Encoder 3")
+        last_position_4 = handle_encoder(encoder_4, last_position_4, "Encoder 4")
 
-    # Handle Rotary Encoder 1 (track rotation and send arrow keys)
-    # last_position_1 = handle_encoder(encoder_1, last_position_1, "Encoder 1")
-    last_position_2 = handle_encoder(encoder_2, last_position_2, "Encoder 2")
-    last_position_3 = handle_encoder(encoder_3, last_position_3, "Encoder 3")
+        # Delay to prevent multiple fast triggers
+        time.sleep(0.01)
 
-    # Delay to prevent multiple fast triggers
-    time.sleep(0.01)
+
+if __name__ == "__main__":
+    main()
