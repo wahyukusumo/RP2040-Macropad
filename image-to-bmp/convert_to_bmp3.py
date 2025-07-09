3eedfc2a805455b81f476bef53a1d8e467ac8103
Wahyu Kusumo <dev@wahyukusumo.com>
2025-07-09 07:24:40 +0700

    Add multiple way to convert image to bmp3

---
+++ image-to-bmp/convert_to_bmp3.py
@@ -0,0 +1,12 @@
+# https://github.com/todbot/circuitpython-tricks#python-using-pil--pillow
+# The Python Image Library (PIL) fork pillow seems to work the best. It's unclear how to toggle compression.
+
+from PIL import Image
+
+
+size = (240, 240)
+num_colors = 64
+img = Image.open("myimage.jpg")
+img = img.resize(size)
+newimg = img.convert(mode="P", colors=num_colors)
+newimg.save("myimage_for_cirpy.bmp")
