2d78fa92c730363feb51cb6a5ac02271dc4369ca
Wahyu Kusumo <dev@wahyukusumo.com>
2025-07-13 07:12:39 +0700

    Add function to turn digital value to analog value since deej will only read analog value and turn it to digital value. Also move numbers to global variable.

--- deej.py
+++ deej.py
@@ -1,47 +1,53 @@
 import os
 
+DEFAULT_DIGITAL_VOLUME = 50
+MIN_DIGITAL_VOLUME = 0
+MAX_DIGITAL_VOLUME = 100
+MAX_ANALOG_VOLUME = 1023
+
 
 class Deej:
     def __init__(self, programs: list):
-        self.current = 0
         self.programs = programs  # Display name for program in list
         self.num_programs = len(programs)
-        self.volumes = self.create_or_get_volumes()
+        self.volumes = [DEFAULT_DIGITAL_VOLUME for i in self.programs]
+        self.current = 0
         self.display = self.refresh_display()
 
     def create_or_get_volumes(self):
         filename = "deej.csv"
-        if filename not in os.listdir("."):
+        if filename not in os.listdir("/"):
             with open(filename, "w") as f:
-                data = [50 for i in range(self.num_programs)]
-                f.write(",".join(str(v) for v in data))
+                data = ",".join(str(DEFAULT_DIGITAL_VOLUME) for i in self.num_programs)
+                f.write(data)
         else:
             with open(filename, "r") as file:
-                data = file.readline().strip().split(",")
-
-        return data
+                self.volumes = file.strip().split(",")
 
     def refresh_display(self):
         return f"{self.volumes[self.current]}\n{self.programs[self.current]}"
 
-    def cycle_programs(self, direction):
+    def cycle_programs(self, direction: int):
         # direction value is +1 or -1
         self.current = (self.current + direction) % self.num_programs
         # print(f"current = {self.current}")
         self.display = self.refresh_display()
 
-    def clamp(self, val, min_val=0, max_val=100):
+    def clamp(self, val, min_val=MIN_DIGITAL_VOLUME, max_val=MAX_DIGITAL_VOLUME):
         return max(min_val, min(val, max_val))
 
-    def change_volume(self, direction):
+    def analog_value(self, value: int):
+        return round(value * MAX_ANALOG_VOLUME / MAX_DIGITAL_VOLUME)
+
+    def send_to_serial(self):
+        analog_value = [self.analog_value(v) for v in self.volumes]
+        volumes = map(str, analog_value)
+        serialize = "|".join(volumes)
+        print(serialize)
+
+    def change_volume(self, direction: int):
         # direction value is +2 or -2
         program_volume = self.volumes[self.current]
         self.volumes[self.current] = self.clamp(program_volume + direction)
-        print(self.volumes)
+        self.send_to_serial()
         self.display = self.refresh_display()
-
-
-if __name__ == "__main__":
-    programs = ["Master", "Firefox", "Spotify", "Discord", "Apex Legends"]
-    deej = Deej(programs)
-    print(deej.volumes)
